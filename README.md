67cb213d491bb3a6776122341d0f721485a4c91a
olyesung <olyesung@gmail.com>
2021-08-23 18:31:02 +0900

    wetube2021

---
+++ README.md
@@ -0,0 +1,20 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit My Profile
+/users/delete -> Delete My Profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
+
+<!--
+/videos/comments -> Commet on a Video
+/videos/comments/delets -> Delete A Commet of a Video -->
---
+++ src/routers/userRouter.js
@@ -0,0 +1,37 @@
+import express from "express";
+import {
+  getEdit,
+  postEdit,
+  logout,
+  startGithubLogin,
+  finishGithubLogin,
+  getChangePassword,
+  postChangePassword,
+  see,
+} from "../controllers/userController";
+import {
+  protectorMiddleware,
+  publicOnlyMiddleware,
+  avatarUpload,
+} from "../middlewares";
+
+const userRouter = express.Router();
+
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(avatarUpload.single("avatar"), postEdit);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleware)
+  .get(getChangePassword)
+  .post(postChangePassword);
+
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+
+userRouter.get("/:id", see);
+
+export default userRouter;
