98a3fa4a1b716abf047a2f2636d8d8591290a629
olyesung <olyesung@gmail.com>
2021-08-26 13:23:57 +0900

    post edit modify

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -46,6 +46,8 @@ export const postEdit = async (req, res) => {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
+    console.log(video.owner);
+    console.log(_id);
     req.flash("error", "You are not the the owner of the video.");
     return res.status(403).redirect("/");
   }
