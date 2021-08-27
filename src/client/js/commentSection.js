75c47c869e337a17ff1542b98cc4efe78c55fd07
olyesung <olyesung@gmail.com>
2021-08-27 18:00:50 +0900

    name in comment(28)

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -9,7 +9,7 @@ const addComment = (text, id, username) => {
   const icon = document.createElement("i");
   icon.className = "fas fa-comment";
   const userName = document.createElement("a");
-  console.log(text, id, comment, user);
+  console.log(text, id, video.dataset.id);
   // userName.href = `/users/${comment.owner._id}`;
   userName.innerText = "💥";
   // userName.innerText = ` ${comment.name}`;
