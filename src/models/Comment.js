97947adf429a8c6054369e02cb3cca376ae34cb2
olyesung <olyesung@gmail.com>
2021-08-27 14:11:34 +0900

    name in comment(4)

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -4,6 +4,7 @@ const commentSchema = new mongoose.Schema({
   text: { type: String, required: true },
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   video: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Video" },
+  name: { type: String, required: true },
   createdAt: { type: Date, required: true, default: Date.now },
 });
 
